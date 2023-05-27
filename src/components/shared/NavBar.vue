d36b9aabef4f5b9f11b8b6e7f84a03597471694d
George A <sg1asgard@outlook.com>
2023-05-27 21:12:45 +0300

    quick search

--- src/components/shared/NavBar.vue
+++ src/components/shared/NavBar.vue
@@ -1,6 +1,18 @@
 <script setup></script>
 <template>
-  <div>nav bar</div>
+  <div class="d-flex flex-row">
+    <div class="stema-romaniei">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/7/70/Coat_of_arms_of_Romania.svg" alt="">
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.stema-romaniei {
+  width: 100px;
+  img {
+    width: 100%;
+    height: auto;
+  }
+}
+</style>
