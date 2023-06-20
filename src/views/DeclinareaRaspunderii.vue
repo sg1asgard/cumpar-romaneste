f4ae360bf06dbbd40d22c22126da90ce2f2fed40
George A <sg1asgard@outlook.com>
2023-06-20 18:23:50 +0300

    latest updates

--- src/views/DeclinareaRaspunderii.vue
+++ src/views/DeclinareaRaspunderii.vue
@@ -11,13 +11,175 @@ import PageHeader from '@/components/shared/PageHeader.vue'
       class="has-separator"
     />
     <main>
-      <h3 class="mt-5 text-capitalize">Politica de Confidentialitate</h3>
-      <div>...</div>
+      <h1 class="mt-5 text-capitalize">Declinarea răspunderii</h1>
+
+      <p>Ultima actualizare: 20 iunie 2023</p>
+      <h1>Interpretare și definiții</h1>
+      <h2>Interpretare</h2>
+      <p>
+        Cuvintele a căror litera inițială este scrisă cu majuscule au
+        semnificații definite în următoarele condiții. Următoarele definiții vor
+        avea același înțeles, indiferent dacă apar la singular sau la plural.
+      </p>
+      <h2>Definiții</h2>
+      <p>În scopul acestei clauze de declinare a răspunderii:</p>
+      <ul>
+        <li>
+          <strong>Companie</strong> (denumită fie „Compania”, „Noi”, „Noi” sau
+          „Noștri” în această clauză de declinare a răspunderii) se referă la
+          Cumpar Romaneste.
+        </li>
+        <li><strong>Serviciul</strong> se referă la site-ul web.</li>
+        <li>
+          <strong>Dvs.</strong> înseamnă persoana care accesează Serviciul, sau
+          compania sau altă entitate juridică în numele căreia respectiva
+          persoană accesează sau folosește Serviciul, după caz.
+        </li>
+        <li>
+          <strong>Site-ul web</strong> se referă la Cumpar Romaneste, accesibil
+          de pe
+          <a
+            href="https://cumpar-romaneste.com"
+            rel="external nofollow noopener"
+            target="_blank"
+            >https:// cumpar-romaneste.com</a
+          >
+        </li>
+      </ul>
+      <h1>Declinarea răspunderii</h1>
+      <p>Informațiile conținute în Serviciu au doar scop informativ general.</p>
+      <p>
+        Compania nu își asumă nicio responsabilitate pentru erorile sau
+        omisiunile din conținutul Serviciului.
+      </p>
+      <p>
+        În nici un caz, Compania nu va fi răspunzătoare pentru orice daune
+        speciale, directe, indirecte, consecutive sau incidentale sau orice
+        daune de orice fel, indiferent dacă sunt într-o acțiune contractuală,
+        neglijență sau alt delict, care decurg din sau în legătură cu utilizarea
+        a Serviciului sau a conținutului Serviciului. Compania își rezervă
+        dreptul de a face adăugiri, ștergere sau modificări la conținutul
+        Serviciului în orice moment, fără notificare prealabilă. Această
+        declinare a responsabilității a fost creată cu ajutorul
+        <a
+          href="https://www.freeprivacypolicy.com/free-disclaimer-generator/"
+          target="_blank"
+          >Generatorul gratuit de declinare a responsabilității</a
+        >.
+      </p>
+      <p>
+        Compania nu garantează că Serviciul este lipsit de viruși sau alte
+        componente dăunătoare.
+      </p>
+      <h1>Declinarea răspunderii pentru linkuri externe</h1>
+      <p>
+        Serviciul poate conține link-uri către site-uri web externe care nu sunt
+        furnizate sau întreținute de către sau afiliate în vreun fel cu
+        Compania.
+      </p>
+      <p>
+        Vă rugăm să rețineți că Compania nu garantează acuratețea, relevanța,
+        actualitatea sau caracterul complet al oricărei informații de pe aceste
+        site-uri web externe.
+      </p>
+      <h1>Declinarea răspunderii pentru erori și omisiuni</h1>
+      <p>
+        Informațiile oferite de Serviciu sunt doar cu titlu de orientare
+        generală în chestiuni de interes. Chiar dacă Compania ia toate măsurile
+        de precauție pentru a se asigura că conținutul Serviciului este atât
+        actual, cât și exact, pot apărea erori. În plus, având în vedere natura
+        în schimbare a legilor, regulilor și reglementărilor, pot exista
+        întârzieri, omisiuni sau inexactități în informațiile conținute în
+        Serviciu.
+      </p>
+      <p>
+        Compania nu este responsabilă pentru erori sau omisiuni sau pentru
+        rezultatele obținute din utilizarea acestor informații.
+      </p>
+      <h1>Declinarea răspunderii pentru utilizarea loială</h1>
+      <p>
+        Compania poate folosi materiale protejate prin drepturi de autor care nu
+        au fost întotdeauna autorizate în mod specific de către proprietarul
+        drepturilor de autor. Compania pune la dispoziție un astfel de material
+        pentru critici, comentarii, știri, predare, burse sau cercetare.
+      </p>
+      <p>
+        Compania consideră că aceasta constituie o „utilizare loială”; a
+        oricărui astfel de material protejat prin drepturi de autor, așa cum
+        este prevăzut în secțiunea 107 din legea drepturilor de autor din
+        Statele Unite.
+      </p>
+      <p>
+        Dacă doriți să utilizați material protejat prin drepturi de autor de la
+        Serviciu în scopuri proprii, care depășesc utilizarea loială, trebuie să
+        obțineți permisiunea proprietarului drepturilor de autor.
+      </p>
+      <h1>Declinarea răspunderii exprimată de afișări</h1>
+      <p>
+        Serviciul poate conține opinii și opinii care aparțin autorilor și nu
+        reflectă neapărat politica sau poziția oficială a oricărui alt autor,
+        agenție, organizație, angajator sau companie, inclusiv a Companiei.
+      </p>
+      <p>
+        Comentariile publicate de utilizatori sunt responsabilitatea exclusivă a
+        acestora, iar utilizatorii își vor asuma întreaga responsabilitate,
+        răspunderea și vina pentru orice calomnie sau litigiu care rezultă din
+        ceva scris sau ca rezultat direct al ceva scris într-un comentariu.
+        Compania nu este responsabilă pentru niciun comentariu publicat de
+        utilizatori și își rezervă dreptul de a șterge orice comentariu din
+        orice motiv.
+      </p>
+      <h1>Declinarea răspunderii fără responsabilitate</h1>
+      <p>
+        Informațiile despre Serviciu sunt furnizate cu înțelesul că Compania nu
+        este angajată aici în furnizarea de consiliere și servicii juridice,
+        contabile, fiscale sau de altă natură profesională. Ca atare, nu ar
+        trebui să fie folosit ca un substitut pentru consultarea cu
+        profesioniști contabili, fiscali, juridici sau alți consilieri
+        competenți.
+      </p>
+      <p>
+        În nici un caz, Compania sau furnizorii săi nu vor fi răspunzători
+        pentru orice daune speciale, incidentale, indirecte sau consecutive care
+        decurg din sau în legătură cu accesul sau utilizarea sau incapacitatea
+        dvs. de a accesa sau utiliza Serviciul.
+      </p>
+      <h1>&quot;Folosiți pe propriul risc&quot; Disclaimer</h1>
+      <p>
+        Toate informațiile din Serviciu sunt furnizate „ca atare”, fără nicio
+        garanție privind integralitatea, acuratețea, actualitatea sau
+        rezultatele obținute din utilizarea acestor informații și fără garanții
+        de niciun fel, exprestrâns sau implicit, inclusiv, dar fără a se limita
+        la garanții de performanță, vandabilitate și potrivire pentru un anumit
+        scop.
+      </p>
+      <p>
+        Compania nu va fi răspunzătoare față de Dvs. sau de oricine altcineva
+        pentru orice decizie luată sau acțiune întreprinsă pe baza informațiilor
+        furnizate de Serviciu sau pentru orice daune consecutive, speciale sau
+        similare, chiar dacă este informată cu privire la posibilitatea unor
+        astfel de daune.
+      </p>
+      <h1>Contactați-ne</h1>
+      <p>
+        Dacă aveți întrebări despre această clauză de declinare a răspunderii,
+        ne puteți contacta:
+      </p>
+      <ul>
+        <li>
+          Prin accesarea acestei pagini de pe site-ul nostru:
+          <a
+            href="https://cumpar-romaneste.ro"
+            rel="external nofollow noopener"
+            target="_blank"
+            >https://cumpar-romaneste.ro</a
+          >
+        </li>
+      </ul>
     </main>
 
     <FooterNav />
   </div>
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
